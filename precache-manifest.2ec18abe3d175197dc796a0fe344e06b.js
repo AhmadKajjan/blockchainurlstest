3b0f86e9e740fcad9b924cd129c381cfd058eb22
Ahmad-Kajjan <blaketear@hotmail.com>
2020-06-23 13:16:41 -0700

    Updates

---
+++ precache-manifest.2ec18abe3d175197dc796a0fe344e06b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c755c4b03ac243a9f2f8b9bb20a4952",
+    "url": "/blockchainurlstest/index.html"
+  },
+  {
+    "revision": "674cbf3419236beb23af",
+    "url": "/blockchainurlstest/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f85bd0c4f23e4fae949c",
+    "url": "/blockchainurlstest/static/js/2.5605ff02.chunk.js"
+  },
+  {
+    "revision": "2408d2d273034150b0f0c01517c6f59a",
+    "url": "/blockchainurlstest/static/js/2.5605ff02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "674cbf3419236beb23af",
+    "url": "/blockchainurlstest/static/js/main.771f487b.chunk.js"
+  },
+  {
+    "revision": "15360d9246e89246962d",
+    "url": "/blockchainurlstest/static/js/runtime-main.7ec07972.js"
+  }
+]);
